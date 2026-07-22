733dc36589eb94b6042d6232bd2eb985381eefa8
Ezrabro <67427897+Ezrabro@users.noreply.github.com>
2026-07-22 22:23:37 +0330

    made the rgb contorlls better

--- keyboards/keychron/k5_version_2/iso/rgb/keymaps/azar/rules.mk
+++ keyboards/keychron/k5_version_2/iso/rgb/keymaps/azar/rules.mk
@@ -2,4 +2,14 @@ CAPS_WORD_ENABLE = yes
 MOUSEKEYS_ENABLE = no
 RGB_MATRIX_CUSTOM_USER = yes
 
+# no free up some space
+RGBLIGHT_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
+
+
 SRC += features/calculator.c
